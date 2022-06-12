70050678c79cebf5b4a6dce6977c25e24e5dad06
Ruthenium0 <54074649+Ruthenium0@users.noreply.github.com>
2022-06-12 16:52:24 +0300

    Update RegisterView.js

--- Lab.2/js/view/RegisterView.js
+++ Lab.2/js/view/RegisterView.js
@@ -1,1 +1,16 @@
+export default class RegisterView {
+	constructor(itemModel) {
+		this.itemModel = itemModel;
+		this.controllerOnUpdate = null;
+
+	}
+
+	onUpdate(val) {
+		document.getElementById("submit").disabled = val;
+	}
+
+	setOnChangeCallback(controllerOnUpdate) {
+		this.controllerOnUpdate = controllerOnUpdate;
+	}
+}
 
