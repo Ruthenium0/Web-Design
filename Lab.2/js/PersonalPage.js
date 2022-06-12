cc8eee216d4f805299b35b0ef036846d60903579
Ruthenium0 <54074649+Ruthenium0@users.noreply.github.com>
2022-06-12 16:51:22 +0300

    Update PersonalPage.js

--- Lab.2/js/PersonalPage.js
+++ Lab.2/js/PersonalPage.js
@@ -1,1 +1,13 @@
+import PersonalPageModel from './model/PersonalPageModel.js';
+import PersonalPageView from './view/PersonalPageView.js';
+import PersonalPageController from './controller/PersonalPageController.js';
+
+
+var PersonalPageModel = new PersonalPageModel();
+var PersonalPageView = new PersonalPageView(PersonalPageModel);
+
+var controller = new PersonalPageController(PersonalPageModel, PersonalPageView);
+
+
+window.controller = controller
 
